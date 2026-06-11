4eb25ded121e2ab964bea81c65410d396f931b17
Noah Jenner <64815328+Eutropios@users.noreply.github.com>
2026-06-11 00:37:26 -0700

    Fixed treesitter impl and add lualine

--- lua/pack.lua
+++ lua/pack.lua
@@ -9,6 +9,8 @@ vim.pack.add({
 	"https://github.com/saghen/blink.lib",
 	"https://github.com/saghen/blink.pairs",
 	"https://github.com/stevearc/conform.nvim",
+	"https://github.com/lewis6991/gitsigns.nvim",
+	"https://github.com/nvim-lualine/lualine.nvim",
 	{
 		src = "https://github.com/nvim-treesitter/nvim-treesitter",
 		branch = "main",
@@ -54,8 +56,42 @@ cmp.setup({
 	},
 })
 
+require("gitsigns").setup({
+	attach_to_untracked = true,
+	current_line_blame = true,
+	current_line_blame_formatter = "<author>, <author_time:%Y-%m-%d> - <summary>",
+	current_line_blame_opts = {
+		delay = 1000,
+		ignore_whitespace = false,
+		virt_text = true,
+		virt_text_pos = "eol", -- 'eol' | 'overlay' | 'right_align'
+	},
+	linehl = false, -- Toggle with `:Gitsigns toggle_linehl`
+	max_file_length = 40000,
+	numhl = false, -- Toggle with `:Gitsigns toggle_numhl`
+	preview_config = {
+		-- Options passed to nvim_open_win
+		border = "rounded",
+		col = 1,
+		relative = "cursor",
+		row = 0,
+		style = "minimal",
+	},
+	sign_priority = 6,
+	signcolumn = true, -- Toggle with `:Gitsigns toggle_signs`
+	status_formatter = nil, -- Use default
+	update_debounce = 100,
+	watch_gitdir = { interval = 1000, follow_files = true },
+	word_diff = false, -- Toggle with `:Gitsigns toggle_word_diff`
+})
+
 require("blink.pairs").build():pwait(60000)
-require("blink.pairs").setup()
+require("blink.pairs").setup({
+	highlights = {
+		enabled = true,
+		groups = { "BlinkPairsOrange", "BlinkPairsPurple", "BlinkPairsBlue" },
+	},
+})
 require("blink.indent").setup({
 	static = {
 		char = "▏",
@@ -64,6 +100,7 @@ require("blink.indent").setup({
 		char = "▏",
 	},
 })
+
 require("conform").setup({
 	formatters_by_ft = {
 		lua = { "stylua" },
